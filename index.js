b217830bdcaa10032289af3d0018592952d25d8a
markovich692 <mark.devguru@outlook.com>
2025-05-09 21:04:08 -0500

    solves the challenge

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //Write a function that takes an array of numbers and
-// returns the sum of the numbers. The numbers can be negative or non-integer.
-//  If the array does not contain any numbers then you should return 0.
+//returns the sum of the numbers. The numbers can be negative or non-integer.
+//If the array does not contain any numbers then you should return 0.
+
+function sum(numbers) {
+  if (!numbers.length) return 0;
+
+  return numbers.reduce((acc, num) => {
+    return acc + num;
+  }, 0);
+}

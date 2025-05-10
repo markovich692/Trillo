8aa3b2a5430e23082c2c529235136159e8ea0946
markovich692 <mark.devguru@outlook.com>
2025-05-09 21:03:06 -0500

    starts to solve codewars challenge

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Deoxyribonucleic acid, DNA is the primary information
-// storage molecule in biological systems. It is composed
-// of four nucleic acid bases Guanine ('G'), Cytosine ('C'),
-//  Adenine ('A'), and Thymine ('T').
-
-// Ribonucleic acid, RNA, is the primary messenger molecule in cells.
-//  RNA differs slightly from DNA its chemical structure and contains no Thymine.
-//  In RNA Thymine is replaced by another nucleic acid Uracil ('U').
-
-// Create a function which translates a given DNA string into RNA.
+//Write a function that takes an array of numbers and
+// returns the sum of the numbers. The numbers can be negative or non-integer.
+//  If the array does not contain any numbers then you should return 0.

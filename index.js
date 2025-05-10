052b01b02d2a0bdf3b57d97768aac59a9a92da44
markovich692 <mark.devguru@outlook.com>
2025-05-09 22:13:00 -0500

    solves the kata

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 // After your function finds the needle it should return a message (as a string) that says:
 
 // "found the needle at position " plus the index it found the needle, so:
+
+const findNeedle = function (arr) {
+  const needlePosition = arr.findIndex((el) => {
+    return el === "needle";
+  });
+
+  if (!needlePosition) return;
+
+  return `found the needle at position ${needlePosition}`;
+};
+
+const testing = findNeedle([
+  "hay",
+  "junk",
+  "hay",
+  "hay",
+  "moreJunk",
+  "needle",
+  "randomJunk",
+]);
+
+console.log(testing);

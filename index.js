010ec4f6ea393c717ad8ee5caf60f8b03d5c7a52
markovich692 <mark.devguru@outlook.com>
2025-05-10 22:36:08 -0500

    starts a new kata

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 // Description:
-// Given a time in a time format class, return
-//  it without year, month and day.
-
-// It should return a string including milliseconds
-//  with 3 decimals.
-
-// Example:
-
-// new Date(2016, 2, 8, 16, 42, 59)
-//Should return:
-// "16:42:59,000"
-
-// function convert(...args) {
-//   console.log(args);
-//   const dateFormat = new Date(...args);
-//   console.log(dateFormat);
-// }
-
-function convert(...time) {
-  const hours = time[3];
-  const minutes = time[4];
-
-  const secs = new Intl.NumberFormat("en-US").format(time[5] * 1000);
-
-  console.log(`${hours}:${minutes}:${secs}`);
-}
-
-convert(2016, 2, 8, 16, 42, 59);
+// Make a program that filters a list of strings and
+// returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be
+// sure that it has to be a friend of yours! Otherwise,
+//  you can be sure he's not...

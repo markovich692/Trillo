56c7f2021716b5d167170dcc0c52b1c73d73f6b2
markovich692 <mark.devguru@outlook.com>
2025-05-11 21:53:32 -0500

    starts a new kata

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// 1
-// 3     5
-// 7     9    11
-// 13    15    17    19
-// 21    23    25    27    29
-// ...
+// Description:
+// The Story:
+// Bob is working as a bus driver. However, he has become extremely
+// popular amongst the city's residents. With so many passengers wanting
+// to get aboard his bus, he sometimes has to face the problem of not
+// enough space left on the bus! He wants you to write a simple program
+// telling him if he will be able to fit all the passengers.
 
-// Calculate the sum of the numbers in the nth row of
-// this triangle (starting at index 1) e.g.: (Input --> Output)
+// Task Overview:
+// You have to write a function that accepts three parameters:
 
-// 1 -->  1
-// 2 --> 3 + 5 = 8
-
-function rowSumOddNumbers(n) {
-  let arr = [];
-
-  for (let i = 0; i < n; i++) {
-    let addNum = i === 0 ? n * n - (n - 1) : arr[i - 1] + 2;
-
-    arr.push(addNum);
-  }
-
-  return arr.reduce((acc, num) => acc + num, 0);
-}
-
-const result = rowSumOddNumbers(2);
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus
+// excluding the driver. If there is enough space, return 0, and
+// if there isn't, return the number of passengers he can't take.

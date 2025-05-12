6afa93ef2f1f41dd4c8a7ab3ed05230889146f96
markovich692 <mark.devguru@outlook.com>
2025-05-11 19:32:52 -0500

    starts a new kata

--- index.js
+++ index.js
@@ -4,3 +4,9 @@
 // If a name has exactly 4 letters in it, you can be
 // sure that it has to be a friend of yours! Otherwise,
 //  you can be sure he's not...
+
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []

fb28b1085457989c9d26dac56f1f5349460f8cf9
markovich692 <mark.devguru@outlook.com>
2025-05-14 22:00:48 -0500

    splits the input and turns it into an array

--- index.js
+++ index.js
@@ -8,3 +8,13 @@
 
 // The parameter of accum is a string which includes
 // only letters from a..z and A..Z.
+
+function accum(s) {
+  // your code
+
+  const splittedString = s.split("");
+
+  console.log(splittedString);
+}
+
+accum("abcd");

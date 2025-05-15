2d63da8026e414a35a7bc148e019e5bd43a3a897
markovich692 <mark.devguru@outlook.com>
2025-05-15 01:20:09 -0500

    starts a new kata

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 // Description:
-// Write a function, persistence, that
-// takes in a positive parameter num and
-// returns its multiplicative persistence,
-//  which is the number of times you must
-//  multiply the digits in num until you
-//  reach a single digit.
+// Given an array (arr) as an argument complete the
+// function countSmileys that should return the total
+//  number of smiling faces.
 
-// For example (Input --> Output):
-// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4
-// and 4 has only one digit, there are 3 multiplications)
+// Rules for a smiling face:
 
-// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12,
-// and finally 1*2 = 2, there are 4 multiplications)
+// Each smiley face must contain a valid pair
+// of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does
+// not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth
+// that should be marked with either ) or D
+// No additional characters are allowed except
+// for those mentioned.
 
-// 4 --> 0 (because 4 is already a one-digit number,
-// there is no multiplication)
-
-function persistence(num) {
-  //code me
-  let add = 0;
-
-  while (num >= 10) {
-    num = num
-      .toString()
-      .split("")
-      .reduce((acc, digit) => acc * Number(digit), 1);
-
-    console.log(num);
-    add++;
-  }
-
-  return add;
-}
-
-persistence(39);
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
